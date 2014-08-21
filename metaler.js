117f7da35ac60c885468375bfdba3337c143f7d9
Justin Endler <justin.endler@brandingbrand.com>
2014-08-21 01:10:48 -0400

    implement normalization of any parameter.

--- metaler.js
+++ metaler.js
@@ -14,6 +14,56 @@ var eventKeys = [
   'extra2'
 ];
 
+// @todo add README.md
+//   list dependencies
+
+function Utility () {}
+Utility.prototype.out = function (noteEvent) {
+  var noteEventArray = [];
+  for (var i = 0, l = eventKeys.length; i < l; i++) {
+    noteEventArray.push(noteEvent[eventKeys[i]]);
+  }
+  outlet(0, noteEventArray);
+};
+Utility.prototype.log = function (l, x) {
+  if (x) {
+    return post(l + ': ' + x + '\n');
+  }
+  post(l + '\n');
+};
+
+var utility = new Utility();
+
+
+// message callbacks
+function dump () {
+  for (var i = 0, l = events.length; i < l; i++) {
+    events[i].post();
+  }
+}
+
+function normalize (key, limit) {
+  var highest = 0;
+  // default, normalize velocity, limit 127
+  if (!key) {
+    key = 'velocity';
+  }
+  if (!limit) {
+    limit = 127;
+  }
+
+  _.each(events, function (noteEvent) {
+    if (noteEvent[key] > highest) {
+      highest = noteEvent[key];
+    }
+  });
+  _.each(events, function (noteEvent) {
+    // value / highest = newValue / limit
+    noteEvent[key] = Math.ceil((limit * noteEvent[key]) / highest);
+  });
+}
+
+
 function NoteEvent (eventArray, eventKeys) {
   // parse array into object properties according to keys
   for (var i = 0, l = eventArray.length; i < l; i++) {
@@ -21,11 +71,11 @@ function NoteEvent (eventArray, eventKeys) {
   }
 }
 
-NoteEvent.prototype.transpose = function transpose (offset) {
+NoteEvent.prototype.transpose = function (offset) {
   this.pitch += offset;
-}
+};
 
-NoteEvent.prototype.toString = function toString () {
+NoteEvent.prototype.toString = function () {
   var str = '{\n';
   for (var key in this) {
     if (this.hasOwnProperty(key)) {
@@ -34,38 +84,16 @@ NoteEvent.prototype.toString = function toString () {
   }
   str = str.slice(0, -1) + '\n}\n';
   return str;
-}
+};
 
-NoteEvent.prototype.post = function eventPost () {
+NoteEvent.prototype.post = function () {
   post(this.toString() + '\n');
-}
+};
 
 // receive midi event
 function list () {
   var noteEvent = new NoteEvent(arrayfromargs(arguments), eventKeys);
-  noteEvent.transpose(14); // @test
   // store event
   events.push(noteEvent);
-  out(noteEvent);
-}
-
-function out (noteEvent) {
-  var noteEventArray = [];
-  for (var i = 0, l = eventKeys.length; i < l; i++) {
-    noteEventArray.push(noteEvent[eventKeys[i]]);
-  }
-  outlet(0, noteEventArray);
-}
-
-function log (l, x) {
-  if (x) {
-    return post(l + ': ' + x + '\n');
-  }
-  post(l + '\n');
-}
-
-function dump () {
-  for (var i = 0, l = events.length; i < l; i++) {
-    events[i].post();
-  }
+  utility.out(noteEvent);
 }
